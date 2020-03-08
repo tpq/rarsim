cebfbeeecd6c1dfc456a718983bca0e9e603059c
Thom Quinn <contacttomquinn@gmail.com>
2020-03-09 10:13:43 +1100

    Add Thompson sampling, but allow for others in future

---
+++ R/4-sampler.R
@@ -0,0 +1,22 @@
+#' @rdname scheduler
+#' @section Samplers:
+#' \code{thompson:} Method to allocate patients by Thompson sampling.
+#'  This function returns an integer corresponding to the group
+#'  to which the patient is randomly allocated.
+#' @export
+thompson <- function(scheduler){
+
+  sch <- scheduler
+
+  # Sample each posterior distribution exactly once
+  sample_each_arm <- sapply(
+    1:sch@K.arms,
+    function(arm){
+      rnorm(1, mean = sch@post.mean[arm], sd = sqrt(sch@post.var[arm]))
+    })
+
+  # Choose allocation group based on the max reward sampled
+  allocation <- which.max(sample_each_arm)
+
+  return(allocation)
+}
