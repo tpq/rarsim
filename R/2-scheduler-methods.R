ab1121395773048a3f3987da709807b3a8d45ed5
Thom Quinn <contacttomquinn@gmail.com>
2020-03-20 14:44:14 +1100

    Now can update with normal-gamma conjugate

--- R/2-scheduler-methods.R
+++ R/2-scheduler-methods.R
@@ -3,11 +3,8 @@
 #' \code{scheduler.start:} Method to initiate a \code{scheduler} object.
 #'  This function returns an updated \code{scheduler} object.
 #' @export
-scheduler.start <- function(prior.mean, prior.var, N.burn.in, sampler = sampler.thompson, heuristic = TRUE){
-
-  if(length(prior.mean) != length(prior.var)){
-    stop("Provide an equal-length vector of prior means and prior variances.")
-  }
+scheduler.start <- function(prior.mean, prior.var, N.burn.in, sampler = sampler.thompson, heuristic = TRUE,
+                            prior.nu, prior.alpha, prior.beta, ...){
 
   if(N.burn.in <= 0){
     stop("Provide a reasonable size for each arm of the 'burn-in' phase.")
@@ -21,20 +18,61 @@ scheduler.start <- function(prior.mean, prior.var, N.burn.in, sampler = sampler.
 
   if(heuristic){
 
+    if(length(prior.mean) != length(prior.var)){
+      stop("Provide an equal-length vector of prior means and prior variances.")
+    }
+
     sch@prior.df <- rep(Inf, length(prior.mean))
     sch@prior.mean <- prior.mean
     sch@prior.var <- prior.var
+    sch@prior.nu <- as.numeric(NA)
+    sch@prior.alpha <- as.numeric(NA)
+    sch@prior.beta <- as.numeric(NA)
 
     sch@heuristic <- TRUE
     sch@post.df <- rep(Inf, length(prior.mean))
+    sch@post.nu <- as.numeric(NA)
+    sch@post.alpha <- as.numeric(NA)
+    sch@post.beta <- as.numeric(NA)
 
   }else{
 
-    # TO UPDATE IN FUTURE VERSION
-    stop("Normal-gamma conjugate prior not yet implemented.")
-    # Note: update @post.var and @df based on alpha, beta
-    message("Alert: The @prior.var slot will contain the variance of the marginal\n",
+    if(missing(prior.beta) | missing(prior.alpha) | missing(prior.nu)){
+
+      if(!missing(prior.var)){
+
+        if(length(prior.mean) != length(prior.var)){
+          stop("Provide an equal-length vector of prior means and prior variances.")
+        }
+
+        message("Alert: Guessing priors for alpha and beta based on 'prior.var'.")
+        message("Alert: Setting prior nu equal to 1.")
+        message("Alert: Setting prior alpha equal to 1.")
+        message("Alert: Setting prior beta equal to 'prior.var'.")
+
+        # NOTE: tau is a gamma distribution, sigma is inverse gamma!
+        prior.nu <- rep(1, length(prior.mean))
+        prior.alpha <- rep(1, length(prior.mean))
+        prior.beta <- prior.var
+
+      }else{
+
+        stop("Provide a prior for variance, or a prior for nu, alpha, and beta.")
+      }
+    }
+
+    # Save all priors
+    sch@heuristic <- FALSE
+    sch@prior.mean <- prior.mean
+    sch@prior.nu <- prior.nu
+    sch@prior.alpha <- prior.alpha
+    sch@prior.beta <- prior.beta
+
+    # These parameters define the marginal t-distribution for the posterior mean
+    message("Alert: The @prior.var and @post.var slots will contain the variance of the marginal\n",
             " distribution of the mean, not the expected variance of the data!")
+    sch@prior.df <- 2 * sch@prior.alpha
+    sch@prior.var <- sch@prior.beta / (sch@prior.nu * sch@prior.alpha)
   }
 
   sch@N.burn.in <- N.burn.in
@@ -43,12 +81,14 @@ scheduler.start <- function(prior.mean, prior.var, N.burn.in, sampler = sampler.
   sch@rewards <- lapply(1:length(prior.mean), function(x) NULL) # rewards are NULL to start
   sch@meta <- list() # for future use...
   sch@sampler.id <- as.character(substitute(sampler)) # get function name...
+  sch@sampler.args <- as.list(substitute(list(...)))[-1]
   sch@sampler <- sampler # the sampler is saved on initialization
   allocation <- rep(1:sch@K.arms, N.burn.in) # burn-in will have an equal allocation ratio
   sch@allocation <- sample(factor(allocation, 1:sch@K.arms)) # make sure first allocation is random
   sch@ingest <- split(rep(NA, length(sch@allocation)), sch@allocation) # dummy structure for first data ingest
   sch@history.post <- data.frame(step = sch@step, arm = 1:sch@K.arms,
                                  mean = sch@prior.mean, var = sch@prior.var, df = sch@prior.df,
+                                 nu = sch@prior.nu, alpha = sch@prior.alpha, beta = sch@prior.beta,
                                  total = 0, next_ratio = 1/sch@K.arms) # initialize history table with priors
   sch@history <- list() # but history list is empty
   return(sch)
@@ -99,32 +139,34 @@ scheduler.update <- function(scheduler, data.ingest, N.allocate){
 
     # Update all posteriors using empiric data
     prior.prec <- 1/sch@prior.var
-    sch@post.mean <- sapply(
-      1:sch@K.arms,
-      function(arm){
-        # (prec0 * mu0 + prec*sum(x)) / (prec0 + n*prec)
-        (prior.prec[arm]*sch@prior.mean[arm] + sch@online.prec[arm]*sch@online.sum[arm])/
-          (prior.prec[arm] + sch@online.count[arm]*sch@online.prec[arm])
-      })
-    post.prec <- sapply(
-      1:sch@K.arms,
-      function(arm){
-        prior.prec[arm] + sch@online.count[arm]*sch@online.prec[arm]
-      })
-    sch@post.var <- sapply(post.prec, function(prec) 1/prec)
+    sch@post.mean <-
+      # (prec0 * mu0 + prec*sum(x)) / (prec0 + n*prec)
+      (prior.prec*sch@prior.mean + sch@online.prec*sch@online.sum)/
+      (prior.prec + sch@online.count*sch@online.prec)
+    post.prec <- prior.prec + sch@online.count*sch@online.prec
+    sch@post.var <- 1/post.prec
 
   }else{
 
-    # TO UPDATE IN FUTURE VERSION
-    stop("Normal-gamma conjugate prior not yet implemented.")
-    # Note: update @post.var and @df based on alpha, beta
-    message("Alert: The @post.var slot will contain the variance of the marginal\n",
-            " distribution of the mean, not the expected variance of the data!")
+    # Update all posteriors using empiric data
+    sch@post.mean <-
+      (sch@prior.nu * sch@prior.mean + sch@online.count * sch@online.mean) /
+      (sch@prior.nu + sch@online.count)
+    sch@post.nu <- sch@prior.nu + sch@online.count
+    sch@post.alpha <- sch@prior.alpha + sch@online.count/2
+    sch@post.beta <-
+      sch@prior.beta + sch@online.count*sch@online.var/2 +
+      (sch@online.count*sch@prior.nu)/(sch@prior.nu + sch@online.count) *
+      (sch@online.mean-sch@prior.mean)^2/2
+
+    # These parameters define the marginal t-distribution for the posterior mean
+    sch@post.df <- 2 * sch@post.alpha
+    sch@post.var <- sch@post.beta / (sch@post.nu * sch@post.alpha)
   }
 
   # Call the sampler (e.g., sampler.thompson) one patient at a time...
   allocation <- sapply(1:N.allocate, function(patient){
-    do.call(sch@sampler, list("scheduler" = sch))
+    do.call(sch@sampler, append(list("scheduler" = sch), sch@sampler.args))
   })
   sch@allocation <- factor(allocation, 1:sch@K.arms) # factor() makes sure that arms with no patients are not forgotten
 
@@ -136,6 +178,7 @@ scheduler.update <- function(scheduler, data.ingest, N.allocate){
     sch@history.post,
     data.frame(step = sch@step, arm = 1:sch@K.arms,
                mean = sch@post.mean, var = sch@post.var, df = sch@post.df,
+               nu = sch@post.nu, alpha = sch@post.alpha, beta = sch@post.beta,
                total = sch@online.count, next_ratio = as.numeric(table(sch@allocation) / sum(table(sch@allocation)))
     )
   )
